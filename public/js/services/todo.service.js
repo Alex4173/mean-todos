3b2879ebbe26d044132f0865fe42fb250914c348
Alex Jones <alex.l.jones.3@gmail.com>
2016-11-14 17:15:37 -0500

    fixed app crash because route function did not terminate on error

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -3,11 +3,40 @@
   angular.module('mean-todos')
         .factory('TodoService', TodoService);
 
-  TodoService.$inject = [];
+  TodoService.$inject = ['$http'];
 
   function TodoService(){
+    init();
+    var todos = [];
     return {
-
+      get: getAllTodos,
+      create: createOneTodo,
+      update: updateOneTodo,
+      delete: deleteOneTodo
     };
+
+    function init(){ //this is going to make our first
+      $http.get('/todos')
+      .then(function(response){
+        todos = response.data.todos;
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    }
+    function getAllTodos(){
+      return todos;
+    }
+    function createOneTodo(todo){
+      $http.post('/todos', todo)
+      .then(function(response){
+        todos.push(todo);
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    }
+    function updateOneTodo(index, todo){}
+    function deleteOneTodo(index){}
   }
 }());
