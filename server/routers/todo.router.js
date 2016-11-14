e9cf7e79d15739011488064c0ea6db9d6612fc3b
Alex Jones <alex.l.jones.3@gmail.com>
2016-11-14 17:21:20 -0500

    fixed app crash because route function did not terminate on error

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -6,25 +6,25 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
-
 router.get('/todos', function(req, res){
   Todo.find({}, function(err, foundTodos){
     if(err){
-      res.status(500).json({
+      return res.status(500).json({ //this stop the function from running further and crashing your application
         err: err
       });
     }
-    res.status(200).json({
+    return res.status(200).json({
       todos: foundTodos
     });
   });
 });
-router.get('/todos/:id', function(req, res){
+router.get('/todos/:id', function(req, res, next){
   Todo.find({_id: req.params.id}, function(err, foundTodo){
     if(err){
       res.status(500).json({
         err: err
       });
+      next(); //this will stop the application from running further in the function and crashing your app
     }
     res.status(200).json({
       todo: foundTodo
@@ -35,18 +35,21 @@ router.post('/todos', function(req, res){
   var todo = new Todo(req.body);
   todo.save(function(err){
     if(err){
+      // throw err; //don't do this! for now...
       res.status(500).json({
         err: err
       });
+    } else { //this will keep the app from trying to send two responses and crashing the application
+      res.status(201).json({
+        msg: 'successfully created todo'
+      });
     }
-    res.status(201).json({
-      msg: 'sucessfully created todo'
-    });
+
   });
 });
 router.put('/todos/:id', function(req, res){
   Todo.findOneAndUpdate({_id: req.params.id}, req.body, function(err, oldTodo){
-    if(err){
+    if(err){ //if left like the following, the function will try to return two response if there is an error, crashing your application
       res.status(500).json({
         err: err
       });
@@ -58,7 +61,7 @@ router.put('/todos/:id', function(req, res){
 });
 router.delete('/todos/:id', function(req, res){
   Todo.findOneAndRemove({ _id: req.params.id }, function(err, deletedTodo){
-    if(err){
+    if(err){ //if left like the following, the function will try to return two response if there is an error, crashing your application
       res.status(500).json({
         err: err
       });
@@ -67,7 +70,18 @@ router.delete('/todos/:id', function(req, res){
       msg: deletedTodo
     });
   });
-
+});
+router.get('/todos/description/:desc', function(req, res){
+  Todo.find({ description: req.params.desc }, function(err, foundTodos){
+    if(err){ //if left like the following, the function will try to return two response if there is an error, crashing your application
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(200).json({
+      todos: foundTodos
+    });
+  });
 });
 
 module.exports = router;
